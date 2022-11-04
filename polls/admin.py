7717b2567ec1d74774409bf2bcd866aa48d9a6f8
Henry <freetodo0212@gmail.com>
2022-11-05 02:49:46 +0900

    bring

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 from django.contrib import admin
 
 from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
